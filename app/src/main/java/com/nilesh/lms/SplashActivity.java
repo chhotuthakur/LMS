407a8cf54885ab691a8cd67e4722105268038275
chhotuthakur <npsdahiya@gmail.com>
2023-05-18 13:16:56 +0530

    added reg fragment and updated account AccountActivity

--- app/src/main/java/com/nilesh/lms/SplashActivity.java
+++ app/src/main/java/com/nilesh/lms/SplashActivity.java
@@ -2,6 +2,7 @@ package com.nilesh.lms;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, AccountActivity.class));
             }
         },4000);
     }
